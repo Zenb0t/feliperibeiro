409533477f2efeec7dc45cb63c75e3a9d70dfaa1
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2023-12-29 09:01:24 -0800

    Update meta description and title tags Refactor App.js and add center alignment to dividers Update project description and button styling in Projects.js Update Hero.js with revised introduction and button styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,50 +1,72 @@
 import {
-    Stack,
-    Flex,
-    Text,
-    VStack,
-    useBreakpointValue,
-    useColorModeValue,
-    HStack,
-    Icon,
-    Link,
-    Heading,
-    Tooltip
+  Stack,
+  Flex,
+  Text,
+  VStack,
+  useBreakpointValue,
+  useColorModeValue,
+  HStack,
+  Icon,
+  Link,
+  Heading,
+  Tooltip,
 } from '@chakra-ui/react';
-import { FaGithub, FaLinkedin, FaRegFileAlt } from 'react-icons/fa';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 export default function AboutMe() {
-    return (
-        <Flex as={'section'} py={useBreakpointValue({ base: 8, md: 20 })} id="about">
-            <VStack
-                w={'full'}
-                justify={'center'}
-                px={useBreakpointValue({ base: 8, md: 10 })}
-            >
-                <Heading py={8} color='orange.500'>
-                    About me
-                </Heading>
-                <Stack maxW={'2xl'} align={'flex-start'} spacing={6}>
-                    <Text
-                        color={useColorModeValue('black', 'white')}
-                        fontSize={useBreakpointValue({ base: 'md', md: 'lg' })}>
-                        I'm a passionate software engineer who loves intellectual challenges. My aim is clean, readable and high-value code,
-                        doing what needs to be done efficiently and elegantly.
-                    </Text>
-                    <Text
-                        color={useColorModeValue('black', 'white')}
-                        fontSize={useBreakpointValue({ base: 'md', md: 'lg' })}>
-                        For the majority of my professional life, I have been a businessperson, which honed my ability to adapt, plan and communicate.
-                        I bring with me domain analysis, problem-solving and conflict management as core skills from my past experiences.
-                    </Text>
-                    <Text
-                        color={useColorModeValue('black', 'white')}
-                        fontSize={useBreakpointValue({ base: 'md', md: 'lg' })}>
-                        On a personal note, I love hiking around BCs parks and mountains,
-                        playing with my daughter, fiddling some jazz and bossa-nova on my guitar and geeking out about physics and quantum computers.
-                    </Text>
-                    <HStack py={8} alignSelf={'center'} spacing={8}>
-                        <Tooltip label="My Resume" aria-label='PDF Resume'>
+  return (
+    <Flex
+      as={'section'}
+      py={useBreakpointValue({ base: 8, md: 20 })}
+      id="about"
+    >
+      <VStack
+        w={'full'}
+        justify={'center'}
+        px={useBreakpointValue({ base: 8, md: 10 })}
+      >
+        <Heading py={8} color="orange.500">
+          About me
+        </Heading>
+        <Stack maxW={'2xl'} align={'flex-start'} spacing={6}>
+          <Text
+            color={useColorModeValue('black', 'white')}
+            fontSize={useBreakpointValue({ base: 'md', md: 'lg' })}
+          >
+            I'm Felipe Ribeiro, a Vancouver-based software developer with a knack
+            for crafting mobile and web applications that address real-world
+            problems. My coding approach is simple: clean, efficient, and
+            value-driven. Transitioning from a business background, I bring a
+            unique perspective to tech, blending strategic planning with
+            technical problem-solving.
+          </Text>
+          <Text
+            color={useColorModeValue('black', 'white')}
+            fontSize={useBreakpointValue({ base: 'md', md: 'lg' })}
+          >
+            These past experiences have shaped my ability to navigate complex
+            projects with a clear vision and communicate effectively with teams
+            and stakeholders. I thrive on turning challenges into opportunities
+            and complex concepts into user-friendly solutions.
+          </Text>
+          <Text
+            color={useColorModeValue('black', 'white')}
+            fontSize={useBreakpointValue({ base: 'md', md: 'lg' })}
+          >
+            Outside of work, I'm usually exploring BC's trails, playing guitar,
+            or diving into the latest in physics, AI and quantum computing. I
+            cherish time with my family and believe in balancing hard work with
+            moments that inspire.
+          </Text>
+          <Text
+            color={useColorModeValue('black', 'white')}
+            fontSize={useBreakpointValue({ base: 'md', md: 'lg' })}
+          >
+            I'm on the lookout for opportunities to collaborate and innovate,
+            aiming to make technology that's as impactful as it is intelligent.
+          </Text>
+          <HStack py={8} alignSelf={'center'} spacing={8}>
+            {/* <Tooltip label="My Resume" aria-label='PDF Resume'>
                             <Link href='' >
                                 <Icon as={FaRegFileAlt} w={12} h={12}
                                     _hover={{ transform: 'scale(1.1)', color: 'cyan.500' }}
@@ -52,29 +74,37 @@ export default function AboutMe() {
                                     transition="all 0.15s ease"
                                 />
                             </Link>
-                        </Tooltip> 
-                        <Tooltip label="LinkedIn" aria-label='LinkedIn'>
-
-                            <Link href='https://www.linkedin.com/in/felipe-ribeiro-245a37192/' isExternal>
-                                <Icon as={FaLinkedin} w={12} h={12}
-                                    _hover={{ transform: 'scale(1.1)', color: 'blue.500' }}
-                                    sx={{ ':hover > svg': { transform: 'scale(1.1)' } }}
-                                    transition="all 0.15s ease"
-                                />
-                            </Link>
-                        </Tooltip>
-                        <Tooltip label="GitHub" aria-label='GitHub'>
-                            <Link href='https://github.com/Zenb0t' isExternal>
-                                <Icon as={FaGithub} w={12} h={12}
-                                    _hover={{ transform: 'scale(1.1)', color: 'purple.500' }}
-                                    sx={{ ':hover > svg': { transform: 'scale(1.1)' } }}
-                                    transition="all 0.15s ease"
-                                />
-                            </Link>
-                        </Tooltip>
-                    </HStack>
-                </Stack>
-            </VStack>
-        </Flex>
-    );
+                        </Tooltip>  */}
+            <Tooltip label="LinkedIn" aria-label="LinkedIn">
+              <Link
+                href="https://www.linkedin.com/in/felipe-ribeiro-245a37192/"
+                isExternal
+              >
+                <Icon
+                  as={FaLinkedin}
+                  w={12}
+                  h={12}
+                  _hover={{ transform: 'scale(1.1)', color: 'blue.500' }}
+                  sx={{ ':hover > svg': { transform: 'scale(1.1)' } }}
+                  transition="all 0.15s ease"
+                />
+              </Link>
+            </Tooltip>
+            <Tooltip label="GitHub" aria-label="GitHub">
+              <Link href="https://github.com/Zenb0t" isExternal>
+                <Icon
+                  as={FaGithub}
+                  w={12}
+                  h={12}
+                  _hover={{ transform: 'scale(1.1)', color: 'purple.500' }}
+                  sx={{ ':hover > svg': { transform: 'scale(1.1)' } }}
+                  transition="all 0.15s ease"
+                />
+              </Link>
+            </Tooltip>
+          </HStack>
+        </Stack>
+      </VStack>
+    </Flex>
+  );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,9 +14,7 @@ export default function HeroWithBackgroundImage({ id }) {
     <Flex
       w={'full'}
       h={'93vh'}
-      backgroundImage={
-        bgImage
-      }
+      backgroundImage={bgImage}
       backgroundSize={'cover'}
       backgroundPosition={'center center'}
       id={id}
@@ -25,36 +23,42 @@ export default function HeroWithBackgroundImage({ id }) {
         w={'full'}
         justify={'center'}
         px={useBreakpointValue({ base: 4, md: 8 })}
-        bgGradient={'linear(to-r, blackAlpha.600, transparent)'}>
+        bgGradient={'linear(to-r, blackAlpha.600, transparent)'}
+      >
         <Stack maxW={'2xl'} align={'flex-start'} spacing={6}>
           <Text
             color={'white'}
             fontWeight={700}
             lineHeight={1.2}
-            fontSize={useBreakpointValue({ base: '3xl', md: '4xl' })}>
-            Hi, I'm Felipe Ribeiro, a full-stack software engineer.
+            fontSize={useBreakpointValue({ base: '3xl', md: '4xl' })}
+          >
+            Hi, I'm Felipe Ribeiro, a full-stack software developer.
           </Text>
           <Text
             color={'white'}
-            fontSize={useBreakpointValue({ base: 'md', md: 'lg' })}>
-            I'm a software engineer based in Vancouver, BC. I have a passion for
-            building web applications and learning new technologies.
+            fontSize={useBreakpointValue({ base: 'md', md: 'lg' })}
+          >
+            With a strategic mindset and insightful approach to technology, I am
+            a Vancouver-based software developer who builds mobile and web
+            applications that solve real challenges and create lasting value.
           </Text>
-          <Stack direction={'row'} >
+          <Stack direction={'row'}>
             <Button
               bg={'whiteAlpha.300'}
               rounded={'full'}
               color={'white'}
-              _hover={{ bg: 'whiteAlpha.500' }}
-              onClick={() => scrollToId('about')}>
+              _hover={{ bg: 'orange.500' }}
+              onClick={() => scrollToId('about')}
+            >
               About me
             </Button>
             <Button
               bg={'whiteAlpha.300'}
               rounded={'full'}
               color={'white'}
-              _hover={{ bg: 'whiteAlpha.500' }}
-              onClick={() => scrollToId('projects')}>
+              _hover={{ bg: 'orange.500' }}
+              onClick={() => scrollToId('projects')}
+            >
               Check my projects
             </Button>
           </Stack>
