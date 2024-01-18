5a43a89dc9158d5364cfc62f207925c3db245510
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2024-01-18 08:26:51 -0800

    Update Hero component with animated text and box

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,18 +2,20 @@ import {
   Stack,
   Flex,
   Button,
-  Text,
   VStack,
   useBreakpointValue,
 } from '@chakra-ui/react';
 import bgImage from '../assets/cup-of-coffee-g32e28e62d_1920.jpg';
 import { scrollToId } from '../utils';
+import { HelloText } from './animated/HelloText';
+import { AnimateText } from './animated/AnimatedText';
+import { AnimateBox } from './animated/AnimatedBox';
 
 export default function HeroWithBackgroundImage({ id }) {
   return (
     <Flex
       w={'full'}
-      h={'93vh'}
+      h={'99vh'}
       backgroundImage={bgImage}
       backgroundSize={'cover'}
       backgroundPosition={'center center'}
@@ -26,42 +28,34 @@ export default function HeroWithBackgroundImage({ id }) {
         bgGradient={'linear(to-r, blackAlpha.600, transparent)'}
       >
         <Stack maxW={'2xl'} align={'flex-start'} spacing={6}>
-          <Text
-            color={'white'}
-            fontWeight={700}
-            lineHeight={1.2}
-            fontSize={useBreakpointValue({ base: '3xl', md: '4xl' })}
-          >
-            Hi, I'm Felipe Ribeiro, a full-stack software developer.
-          </Text>
-          <Text
-            color={'white'}
-            fontSize={useBreakpointValue({ base: 'md', md: 'lg' })}
-          >
-            With a strategic mindset and insightful approach to technology, I am
-            a Vancouver-based software developer who builds mobile and web
+          <HelloText text={"Hi, I'm Felipe"} />
+          <AnimateText delay={4.6}>
+            A Vancouver-based Full Stack Developer. With a strategic mindset and
+            insightful approach to technology, I love building mobile and web
             applications that solve real challenges and create lasting value.
-          </Text>
-          <Stack direction={'row'}>
-            <Button
-              bg={'whiteAlpha.300'}
-              rounded={'full'}
-              color={'white'}
-              _hover={{ bg: 'orange.500' }}
-              onClick={() => scrollToId('about')}
-            >
-              About me
-            </Button>
-            <Button
-              bg={'whiteAlpha.300'}
-              rounded={'full'}
-              color={'white'}
-              _hover={{ bg: 'orange.500' }}
-              onClick={() => scrollToId('projects')}
-            >
-              Check my projects
-            </Button>
-          </Stack>
+          </AnimateText>
+          <AnimateBox delay={4.6}>
+            <Stack direction={'row'} gap={2} pt={2}>
+              <Button
+                bg={'whiteAlpha.300'}
+                rounded={'full'}
+                color={'white'}
+                _hover={{ bg: 'orange.500' }}
+                onClick={() => scrollToId('about')}
+              >
+                About me
+              </Button>
+              <Button
+                bg={'whiteAlpha.300'}
+                rounded={'full'}
+                color={'white'}
+                _hover={{ bg: 'orange.500' }}
+                onClick={() => scrollToId('projects')}
+              >
+                Check my projects
+              </Button>
+            </Stack>
+          </AnimateBox>
         </Stack>
       </VStack>
     </Flex>
